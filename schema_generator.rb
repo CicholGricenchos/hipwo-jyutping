4bbf30fde48835a17a1a8740d9d57b13226b5a19
cichol <cichol@live.cn>
2018-08-13 18:55:29 +0800

    add generated schema

--- schema_generator.rb
+++ schema_generator.rb
@@ -1,4 +1,29 @@
+require 'csv'
+
 class Generator
+  attr_reader :spellers, :translators
+
+  def initialize
+    @spellers = []
+    @translators = []
+    csv = CSV.open('/Users/cichol/Documents/jyut.csv')
+    @data = csv.read[1..-2]
+  end
+
+  def 声组合
+    @data.map do |line|
+      韵 = 分解韵(line[11])
+      if 韵
+        [line[10], *韵]
+      end
+    end.compact.uniq
+  end
+
+  def 分解韵 韵
+    m = 韵.match /^(#{韵腹列表.join('|')})(#{韵尾列表.join('|')})?$/
+    m && [m[1], m[2]]
+  end
+
   def 声母变换
     {
       'ng' => 'v',
@@ -12,9 +37,7 @@ class Generator
       'aa' => 'x',
       'yu' => 'y',
       'eo' => 'v',
-      'oe' => 'v',
-      'ng' => 'n',
-      'm' => 'm'
+      'oe' => 'v'
     }
   end
 
@@ -29,85 +52,102 @@ class Generator
   end
 
   def 韵腹列表
-    %w{aa i u e o a yu oe eo ng m}
+    %w{aa i u e o a yu oe eo}
   end
 
   def 韵尾列表
     %w{i u m n ng p t k}
   end
 
-  def initialize output = STDOUT
-    @output = output
-  end
-
   def 生成变换声母
     声母变换.each do |from, to|
-      output "- xform/^#{from}/#{to.upcase}/"
+      spell "- xform/^#{from}/#{to.upcase}/"
+      translate "- xform/(^|[ '])#{to}/$1#{from.upcase}/"
     end
   end
 
   def 生成原样声母
     声母列表.select{|x| x.size == 1}.each do |x|
-      output "- xform/^#{x}/#{x.upcase}/"
+      spell "- xform/^#{x}/#{x.upcase}/"
     end
   end
 
+  def 带零韵腹
+    声组合.select{|x| x[0].nil?}.map{|x| x[1]}.uniq
+  end
+
   def 生成带零韵腹
-    韵腹列表.each do |from|
+    带零韵腹.each do |from|
       to = 韵腹变换[from] || from
-      output "- xform/^#{from}/#{to.upcase * 2}/"
+      spell "- xform/^#{from}/#{to.upcase * 2}/"
+      translate "- xform/(^|[ '])#{to * 2}/$1#{from.upcase}/"
     end
   end
 
   def 生成变换韵腹
     韵腹变换.each do |from, to|
-      # 鼻音独立韵必须配合零声母使用
-      next if from == 'ng'
-      next if from == 'm'
-      output "- xform/#{from}/#{to.upcase}/"
+      spell "- xform/#{from}/#{to.upcase}/"
+      translate "- xform/(\\w)#{to}/$1#{from.upcase}/"
     end
   end
 
   def 生成原样韵腹
     韵腹列表.select{|x| x.size == 1}.each do |x|
-      output "- xform/#{x}/#{x.upcase}/"
+      spell "- xform/#{x}/#{x.upcase}/"
     end
   end
 
   def 生成变换韵尾
     韵尾变换.each do |from, to|
-      output "- xform/#{from}$/#{to.upcase}/"
+      spell "- xform/#{from}$/#{to.upcase}/"
+      translate "- xform/(\\w)#{to}/$1#{from.upcase}/"
     end
   end
 
   def 生成原样韵尾
     韵尾列表.select{|x| x.size == 1}.each do |x|
-      output "- xform/#{x}$/#{x.upcase}/"
+      spell "- xform/#{x}$/#{x.upcase}/"
     end
   end
 
   def 生成大小写变换
-    output "- xlit/#{('A'..'Z').to_a.join}/#{('a'..'z').to_a.join}/"
+    spell "- xlit/#{('A'..'Z').to_a.join}/#{('a'..'z').to_a.join}/"
+    translate "- xlit/#{('A'..'Z').to_a.join}/#{('a'..'z').to_a.join}/"
   end
 
   def 生成声母衍生
-    output "- derive/J/Y/"
+    spell "- derive/J/Y/"
+    translate "- xform/(^|[ '])y/$1J/"
   end
 
   def 生成韵腹衍生
-    output "- derive/X/H/"
+    spell "- derive/X/H/"
+    translate "- xform/(\\w)h/$1AA/"
   end
 
   def 生成特例
-    output "- derive/^hng$/HN/"
-    output "- derive/^aa$/AA/"
+    spell "- derive/^hng$/HN/"
+    spell "- derive/^aa$/AA/"
+    spell "- xform/^hng$/HG/"
+    spell "- xform/^m$/MM/"
+    spell "- xform/^ng$/NN/"
+
+    translate "- xform/(^|[ '])hg/$1HNG/"
+    translate "- xform/(^|[ '])hn/$1HNG/"
+    translate "- xform/(^|[ '])mm/$1M/"
+    translate "- xform/(^|[ '])nn/$1NG/"
+    translate "- xform/(^|[ '])aa/$1AA/"
   end
 
-  def output line
-    @output.puts line
+  def spell line
+    @spellers << line
   end
 
-  def generate_speller
+  def translate line
+    @translators << line
+  end
+
+  def generate
     生成特例
     生成变换声母
     生成原样声母
@@ -119,11 +159,12 @@ class Generator
     生成变换韵尾
     生成原样韵尾
     生成大小写变换
-  end
-
-  def generate
-    generate_speller
+    self
   end
 end
 
-Generator.new.generate
+puts "***** SPELLERS *****"
+puts Generator.new.generate.spellers
+puts
+puts "***** TRANSLATORS *****"
+puts Generator.new.generate.translators
